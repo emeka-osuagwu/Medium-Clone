78fd7cd75e5d88509f613e0999dc9c86243c9571
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2019-03-07 17:38:23 +0100

    add tag seeder

---
+++ database/seeds/TagTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagTableSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	factory(App\Models\Tag::class, 100)->create();
+    }
+}
+
+
